831fc58982c4db8eb8471fa0cea0937dbe3a5bab
Deepanshu Mishra <49188002+deepanshu016@users.noreply.github.com>
2024-06-26 00:36:01 +0530

    First Commit

--- resources/views/pages/mess_owner/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('pages.layout.layout')
-@section('title','Mess App | Dashboard')
-@section('content')
-<main class="main-wrapper">
-    <div class="container-fluid">
-
-    </div>
-</main>
-@endsection
